bb9b666d3abbf26960ceddc74db959cc49b9e5c7
Brandon Newton <sandalsboy@gmail.com>
2015-03-15 18:15:53 -0700

    downcase a users email before their record is persisted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ActiveRecord::Base
+	before_create :normalize_email
+
   authenticates_with_sorcery!
 
 	validates_presence_of :username, :email, :password, :password_confirmation
 
 	validates :password, confirmation: true
+
+	private
+
+	def normalize_email
+		self.email.downcase!
+	end
 end
