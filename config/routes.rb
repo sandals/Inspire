4984a2f008fa82079dfc4908e0f0b71a0dfd3a67
Brandon Newton <sandalsboy@gmail.com>
2015-03-15 20:26:46 -0700

    Display Image
    Fixed image display on card#show. Tidied up the routes a bit.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 	root to: "static_pages#index", via: :get
 
 	resources :sessions, only: [:new, :create]
+	get "login" => "sessions#new", as: :login
 	post "logout" => "sessions#destroy", as: :logout
 	resources :users
 	resources :cards
